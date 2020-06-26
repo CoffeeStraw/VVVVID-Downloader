b4b10f261647db60957c17dd90385b029e3fee47
CoffeeStraw <clarantonio98@gmail.com>
2020-06-26 18:12:16 +0200

    Aesthetics, formatted code with autopep8

--- text_utility.py
+++ text_utility.py
@@ -6,10 +6,11 @@ GitHub: https://github.com/CoffeeStraw/VVVVID-Downloader
 from platform import system
 from re import sub as re_sub
 
+
 def os_fix_filename(text):
-	'''
-	Correct the text if the script is running from Microsoft OS
-	'''
-	if system() == 'Windows':
-		return re_sub(r'[\\\/\:\*\?\"\<\>\|]+', '', text)
-	return text
+    '''
+    Correct the text if the script is running from Microsoft OS
+    '''
+    if system() == 'Windows':
+        return re_sub(r'[\\\/\:\*\?\"\<\>\|]+', '', text)
+    return text

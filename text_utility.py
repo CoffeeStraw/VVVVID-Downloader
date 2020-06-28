9e925e7e8387fd4fec71e11837907514b798b03e
CoffeeStraw <clarantonio98@gmail.com>
2020-06-28 18:57:41 +0200

    Formatted code using black

--- text_utility.py
+++ text_utility.py
@@ -1,8 +1,8 @@
-'''
+"""
 VVVVID Downloader - Text Utility Functions
 Author: CoffeeStraw
 GitHub: https://github.com/CoffeeStraw/VVVVID-Downloader
-'''
+"""
 from platform import system
 from re import sub as re_sub
 
@@ -12,6 +12,6 @@ def os_fix_filename(filename):
 
     Ref: https://stackoverflow.com/questions/1976007/what-characters-are-forbidden-in-windows-and-linux-directory-names/31976060#31976060
     """
-    if system() == 'Windows':
-        return re_sub(r'[\<\>\:\"\/\\\|\?\*]+', '', filename)
-    return re_sub(r'[\/]+', '\\', filename)
+    if system() == "Windows":
+        return re_sub(r"[\<\>\:\"\/\\\|\?\*]+", "", filename)
+    return re_sub(r"[\/]+", "\\", filename)
--- vvvvid_scraper.py
+++ vvvvid_scraper.py
@@ -1,28 +1,28 @@
-'''
+"""
 VVVVID Downloader - VVVVID Scraper Utility Functions
 Author: CoffeeStraw
 GitHub: https://github.com/CoffeeStraw/VVVVID-Downloader
-'''
+"""
 import re
 from copy import deepcopy
 
 
 def parse_url(url):
-    '''
+    """
     Parse a given link to extract show_id and content name (url formatted)
-    '''
+    """
     # Compatibility with old link format
-    url = url.replace("/#!show/", '/show/')
+    url = url.replace("/#!show/", "/show/")
     # Parsing URL
     pattern = r"show/([0-9]+)/(.+?)/"
     return re.search(pattern, url).groups()
 
 
 def convert_text_to_url_format(text):
-    '''
+    """
     Format a text correctly for the url concatenation
-    '''
-    text = re.sub(r'[^a-zA-Zàèéìòù\s\-\']', '', text)
+    """
+    text = re.sub(r"[^a-zA-Zàèéìòù\s\-\']", "", text)
 
     text = text.replace("à", "a")
     text = re.sub("è|é", "e", text)
@@ -30,42 +30,46 @@ def convert_text_to_url_format(text):
     text = text.replace("ò", "o")
     text = text.replace("ù", "u")
 
-    text = re.sub(r'[\s\']+', '-', text)
+    text = re.sub(r"[\s\']+", "-", text)
     return text.lower()
 
 
 def get_content_infos(requests_obj, show_id):
-    '''
+    """
     Retrieves some informations for the content to beautify output,
     specifically description and well formatted name
-    '''
-    infos_url = 'https://www.vvvvid.it/vvvvid/ondemand/' + show_id + '/info/'
-    json_file = requests_obj['session'].get(
-        infos_url,
-        headers=requests_obj['headers'],
-        params=requests_obj['payload']
-    ).json()
+    """
+    infos_url = "https://www.vvvvid.it/vvvvid/ondemand/" + show_id + "/info/"
+    json_file = (
+        requests_obj["session"]
+        .get(infos_url, headers=requests_obj["headers"], params=requests_obj["payload"])
+        .json()
+    )
 
-    return json_file['data']['title'], json_file['data']['description']
+    return json_file["data"]["title"], json_file["data"]["description"]
 
 
 def get_seasons(requests_obj, url, show_id, url_name):
-    '''
+    """
     Returns a dictionary containing seasons with url
-    '''
+    """
     # Downloading episodes informations
-    json_file = requests_obj['session'].get(
-        "https://www.vvvvid.it/vvvvid/ondemand/" + show_id + "/seasons/",
-        headers=requests_obj['headers'],
-        params=requests_obj['payload']
-    ).json()
+    json_file = (
+        requests_obj["session"]
+        .get(
+            "https://www.vvvvid.it/vvvvid/ondemand/" + show_id + "/seasons/",
+            headers=requests_obj["headers"],
+            params=requests_obj["payload"],
+        )
+        .json()
+    )
 
     # Extracting seasons from json
     seasons = {}
-    for i, season in enumerate(json_file['data']):
-        seasons[str(json_file['data'][i]['season_id'])] = {
-            'name': json_file['data'][i]['name'],
-            'episodes': json_file['data'][i]['episodes']
+    for i, season in enumerate(json_file["data"]):
+        seasons[str(json_file["data"][i]["season_id"])] = {
+            "name": json_file["data"][i]["name"],
+            "episodes": json_file["data"][i]["episodes"],
         }
 
     # Check if the link is a link to a single episode.
@@ -81,12 +85,12 @@ def get_seasons(requests_obj, url, show_id, url_name):
         seasons_c = deepcopy(seasons)
         for season_id, season in seasons_c.items():
             if not stop and season_id == additional_infos[0]:
-                for j, episode in enumerate(season['episodes']):
-                    if str(episode['video_id']) == str(additional_infos[1]):
+                for j, episode in enumerate(season["episodes"]):
+                    if str(episode["video_id"]) == str(additional_infos[1]):
                         stop = True
                         break
                     else:
-                        del seasons[season_id]['episodes'][0]
+                        del seasons[season_id]["episodes"][0]
             else:
                 del seasons[season_id]
 
